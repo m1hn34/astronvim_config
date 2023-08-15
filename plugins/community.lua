6629aad108e656f810fd4b1f3639251811a5a334
m1hn34 <49846896+m1hn34@users.noreply.github.com>
2023-08-15 22:00:57 +0300

    add: themes & plugin category

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,8 +4,15 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
+  -- Color Schemes
   { import = "astrocommunity.colorscheme.catppuccin" },
+  -- { import = "astrocommunity.colorscheme.folke/tokyonight.nvim" },
+  { import = "astrocommunity.colorscheme.everforest" },
+  { import = "astrocommunity.colorscheme.fluormachine-nvim" },
+  { import = "astrocommunity.colorscheme.kanagawa-nvim" },
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
 
-  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  -- Plugins
+  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.pack.typescript" },
 }
